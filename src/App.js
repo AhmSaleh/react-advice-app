737a913c34f52b4837c692d89554ef759ac54c94
ahmsaleh <ahmedsaleh166@gmail.com>
2022-09-06 03:54:52 +0200

    add axios and call the API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  const fetchAdvice = async () => {
+    const { advice } = await (
+      await axios.get("https://api.adviceslip.com/advice")
+    ).data.slip;
+
+    setAdvice(advice);
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  const getAdviceHandler = () => {
+    fetchAdvice();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="card">
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={getAdviceHandler}>
+          <span>GIVE ME ADVICE!</span>
+        </button>
+      </div>
     </div>
   );
 }
